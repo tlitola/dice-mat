d2863b9c973550270a1c8280891ece33e5e299d2
tlitola <touko@litola.net>
2023-12-27 21:22:56 +0200

    Fix incorrect file path in entrypoint script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,8 +3,8 @@
 set -e
 
 echo "Replacing config from environment..."
-sed -E -i "s/!!!NEXT_PUBLIC_SUPABASE_URL!!!/$NEXT_PUBLIC_SUPABASE_URL/g" .next/static/**/*.js
-sed -E -i "s/!!!NEXT_PUBLIC_SUPABASE_ANON_KEY!!!/$NEXT_PUBLIC_SUPABASE_ANON_KEY/g" .next/static/**/*.js
+sed -E -i "s#!!!NEXT_PUBLIC_SUPABASE_URL!!!#$NEXT_PUBLIC_SUPABASE_URL#g" .next/static/chunks/app/*.js
+sed -E -i "s#!!!NEXT_PUBLIC_SUPABASE_ANON_KEY!!!#$NEXT_PUBLIC_SUPABASE_ANON_KEY#g" .next/static/chunks/app/*.js
 
 echo "Executing command..."
 exec "$@"
